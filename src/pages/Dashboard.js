206ea7c6d8de9007a0adfc27ed02c9f57c90105e
Diego Fernandes <diego.schell.f@gmail.com>
2019-10-31 12:02:12 -0300

    Manhã

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import MapView, { Marker, } from 'react-native-maps';
+import { StyleSheet, Image } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Dashboard() {
+  return (
+    <MapView 
+      showsUserLocation
+      initialRegion={{
+        latitude: -7.1466036,
+        longitude: -34.9516375,
+        latitudeDelta: 0.0122,
+        longitudeDelta: 0.0121,
+      }}
+      style={StyleSheet.absoluteFillObject}
+    >
+      <Marker title="Gabriel Goffi" description="Never fucking stop!" coordinate={{ latitude: -7.1466036, longitude: -34.9516375 }}>
+        <Image style={{ width: 50, height: 50, borderRadius: 25 }} source={{ uri: 'https://pbs.twimg.com/profile_images/778771552712163328/dPVjJD03_400x400.jpg' }} />
+      </Marker>
+    </MapView>
+  );
+}
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+
+export default createAppContainer(
+  createSwitchNavigator({
+    Login,
+    Dashboard,
+  }, {
+    initialRouteName: 'Dashboard'
+  })
+);
